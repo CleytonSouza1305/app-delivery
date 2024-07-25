bb41151681373434095fb4f3e527cab6cce2adc1
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-07-24 22:18:49 -0300

    Criando a tela de login

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-async function renderData() {
-  const url = 'http://localhost:3000/restaurants'
+async function renderData(endPoint) {
+  const url = `http://localhost:3000/${endPoint}`
   const data = await fetch(url).then((response) => response.json())
 
   console.log(data);
 }
 
-renderData()
+renderData('user')
