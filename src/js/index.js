dcb26035f8db5d295e7470778e2c16974d92996f
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-07-29 18:21:02 -0300

    Validando o login

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,36 @@
-async function renderData(endPoint) {
-  const url = `http://localhost:3000/${endPoint}`
-  const data = await fetch(url).then((response) => response.json())
+async function renderDataUser() {
+  const url = `http://localhost:3000/user`
 
-  console.log(data);
+  try {
+    const data = await fetch(url).then((response) => response.json())
+    console.log(data);
+
+    const enterButton = document.getElementById('access-acount-btn')
+    enterButton.addEventListener('click', () => {
+      const emailInput = document.getElementById('email-input')
+      const passwordlInput = document.getElementById('password-input')
+  
+      data.forEach((u) => {
+        if (u.email !== emailInput.value || u.password !== passwordlInput.value) {
+          const inputGroupName = document.querySelector('.input-group-name')
+          const inputGroupAddress = document.querySelector('.input-group-address')
+          const inputGroupConfirmPass = document.querySelector('.input-group-cpass')
+
+          inputGroupName.classList.remove('display')
+          inputGroupAddress.classList.remove('display')
+          inputGroupConfirmPass.classList.remove('display')
+
+          const h2 = document.querySelector('h2')
+
+          h2.textContent('Criar')
+        } else {
+          alert('proxima pagina...')
+        }
+      })
+    })
+  } catch (e) {
+    console.log(`Erro ao consultar API, motivo: ${e}`);
+  }
 }
 
-renderData('user')
+renderDataUser()
