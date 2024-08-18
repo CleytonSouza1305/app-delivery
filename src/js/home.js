21b893c479a4388a496d301d7f4bf9e706fa16c1
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-08-17 23:00:06 -0300

    Inicio da criação dos cards

--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,42 @@
-async function getUserDetails(id) {
-  const url = `http://localhost:3000/user/${id}`
-
+async function randomRestaurantSlider() {
+  const url = `http://localhost:3000/restaurants/`
   const data = await fetch(url).then((r) => r.json())
-  const name = document.querySelector('span')
-  name.innerText = data.name
+
+  try {
+    const container = document.querySelector('.restaunrant-content-slider')
+    console.log(container);
+    
+      for (let i = 0; i < data.length; i++) {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.id = data[i].id
+
+        const restaurantImage = document.createElement('img')
+        restaurantImage.src = data[i].restaurantPhoto
+        restaurantImage.classList.add('image-restaurant')
+
+        const restaunrantName = document.createElement('h2')
+        restaunrantName.innerText = data[i].name
+        restaunrantName.classList.add('restaurant-name')
+
+        const categoryDiv = document.createElement('div')
+        categoryDiv.classList.add('category-div')
+
+        const categories = data[i].category
+        for (let i = 0; i < categories.length; i++) {
+          const p = document.createElement('p')
+          p.innerText = categories[i]
+
+          categoryDiv.append(p)
+        }
+
+        card.append(restaurantImage, restaunrantName, categoryDiv)
+        container.append(card)
+    }
+  } catch (e) { 
+    console.log(`Erro ao executar o código, motivo: ${e}`);
+  }
+  console.log(data);
 }
 
-const id = localStorage.getItem('id')
-getUserDetails(id)
+randomRestaurantSlider()
