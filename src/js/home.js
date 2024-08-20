44c6756a722a442c6d9dfa93199d82715a6b8edf
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-08-20 01:56:05 -0300

    Arrumando o json server

--- src/js/home.js
+++ src/js/home.js
@@ -5,7 +5,7 @@ async function randomRestaurantSlider() {
   try {
     const container = document.querySelector('.restaunrant-content-slider')
     
-      for (let i = 0; i < data.length; i++) {
+      for (let i = 0; i < 8; i++) {
         const card = document.createElement('div')
         card.classList.add('card')
         card.id = data[i].id
@@ -25,6 +25,7 @@ async function randomRestaurantSlider() {
         for (let i = 0; i < categories.length; i++) {
           const p = document.createElement('p')
           p.innerText = categories[i]
+          p.classList.add('category-text')
 
           categoryDiv.append(p)
         }
@@ -32,23 +33,21 @@ async function randomRestaurantSlider() {
         const funcionamentDiv = document.createElement('div')
         const textP = document.createElement('p')
 
+        const houer = document.createElement('p')
+        houer.textContent = data[i].openHour + 'h - ' + data[i].closeHour + 'h'
+        houer.classList.add('houer-restaurant')
+
         if (data[i].isOpen === true) {
           textP.textContent = 'Aberto'
           textP.classList.remove('close')
           textP.classList.add('open')
-          houer.classList.remove('close')
-          houer.classList.add('open')
         } else {
           textP.textContent = 'Fechado'
           textP.classList.remove('open')
           textP.classList.add('close')
-          houer.classList.remove('open')
-          houer.classList.add('close')
         }
 
-        const houer = document.createElement('p')
-        houer.textContent = data[i].openHouer + 'h - ' + data[i].closeHouer + 'h'
-
+        funcionamentDiv.classList.add('funcionament-div')
         funcionamentDiv.append(textP, houer)
 
         card.append(restaurantImage, restaunrantName, funcionamentDiv, categoryDiv)
