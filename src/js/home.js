e374af42ecf40146b1c12a0628567f915269f531
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-08-29 18:23:50 -0300

    cards menu

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,6 @@ let restautantsArr = []
 
 promotionalBannerLoop()
 
-
 async function randomRestaurantSlider() {
   const url = `http://localhost:3000/restaurants/`
   const data = await fetch(url).then((r) => r.json())
@@ -119,55 +118,70 @@ async function  renderData() {
     
     const container = document.querySelector('.cards-all-restaurants')
     for (let i = 0; i < restautantsArr.length; i++) {
+      const menu = restautantsArr[i].menu
+      for (let j = 0; j < menu.length; j++) {
+        const card = document.createElement('div')
+        card.classList.add('card-menu')
+        card.dataset.foodname = restautantsArr[i].name
+        card.dataset.idfood = menu[j].name
 
-      const card = document.createElement('div')
-      card.classList.add('card-all')
-      card.id = restautantsArr[i].id
+        const h2Title = document.createElement('h2')
+        h2Title.classList.add('menu-title')
+        h2Title.innerHTML = `<i class="fa-solid fa-utensils"></i> - "${restautantsArr[i].name}"`
+        const image = document.createElement('img')
+        image.classList.add('menu-image')
+        image.src = menu[j].imageFood
 
-      const restaurantImage = document.createElement('img')
-      restaurantImage.src = restautantsArr[i].restaurantPhoto
-      restaurantImage.classList.add('image-restaurant')
+        const foodName = document.createElement('p')
+        foodName.classList.add('name-menu-food')
+        foodName.textContent = menu[j].name
 
-      const restaunrantName = document.createElement('h2')
-      restaunrantName.innerText = restautantsArr[i].name
-      restaunrantName.classList.add('restaurant-name')
+        const categories = menu[j].foodCategory
+        const div = document.createElement('div')
+        div.classList.add('categories-menu')
 
-      const categoryDiv = document.createElement('div')
-      categoryDiv.classList.add('category-div')
+        for (let c = 0; c < categories.length; c++) {
+          const p = document.createElement('p')
+          p.innerHTML = `<span class="rounded"></span> ${categories[c]}`
 
-      const categories = restautantsArr[i].category
-      for (let i = 0; i < categories.length; i++) {
-        const p = document.createElement('p')
-        p.innerText = categories[i]
-        p.classList.add('category-text')
+          div.append(p)
+        }
 
-        categoryDiv.append(p)
+        const feedBackDiv = document.createElement('div')
+        feedBackDiv.classList.add('feedback-div')
+        const heartIcon = document.createElement('button') 
+        heartIcon.classList.add('heart-btn')
+        heartIcon.innerHTML = `<i class="fa-regular fa-heart"></i>`
+
+        const star01 = document.createElement('button')
+        star01.classList.add('stars')
+        star01.innerHTML = `<i class="fa-regular fa-star"></i>`
+
+        const star02 = document.createElement('button')
+        star02.classList.add('stars')
+        star02.innerHTML = `<i class="fa-regular fa-star"></i>`
+
+        const star03 = document.createElement('button')
+        star03.classList.add('stars')
+        star03.innerHTML = `<i class="fa-regular fa-star"></i>`
+
+        const star04 = document.createElement('button')
+        star04.classList.add('stars')
+        star04.innerHTML = `<i class="fa-regular fa-star"></i>`
+
+        const star05 = document.createElement('button')
+        star05.classList.add('stars')
+        star05.innerHTML = `<i class="fa-regular fa-star"></i>`
+
+        const starDiv = document.createElement('div')
+        starDiv.classList.add('star-div')
+        starDiv.append(star01, star02, star03, star04, star05)
+
+        feedBackDiv.append(starDiv, heartIcon)
+
+        card.append(h2Title, image, foodName,div, feedBackDiv)
+        container.append(card)
       }
-
-      const funcionamentDiv = document.createElement('div')
-      const textP = document.createElement('p')
-
-      const houer = document.createElement('p')
-      houer.textContent = restautantsArr[i].openHour + 'h - ' + restautantsArr[i].closeHour + 'h'
-      houer.classList.add('houer-restaurant')
-
-      const currentDate = new Date().getHours()
-
-      if (currentDate >= restautantsArr[i].openHour && currentDate < restautantsArr[i].closeHour) {
-        textP.textContent = 'Aberto'
-        textP.classList.remove('close')
-        textP.classList.add('open')
-      } else {
-        textP.textContent = 'Fechado'
-        textP.classList.remove('open')
-        textP.classList.add('close')
-      }
-
-      funcionamentDiv.classList.add('funcionament-div')
-      funcionamentDiv.append(textP, houer)
-
-      card.append(restaurantImage, restaunrantName, funcionamentDiv, categoryDiv)
-      container.append(card)
   }
 
   } catch (e) {
